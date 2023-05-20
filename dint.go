693c407146d2d4b80917bf1033fa666252cc38e7
Алексей Ковригин <83507071+alekseikovrigin@users.noreply.github.com>
2023-05-21 01:12:58 +0300

    Add prefix and re-exportability

--- dint.go
+++ dint.go
@@ -5,61 +5,68 @@ import (
 	"time"
 )
 
-// Handling dates in a human-readable integer format (dint).
+// Dint Handling dates in a human-readable integer format (dint).
 // Example: 20230912 = September 12, 2023
 type Dint struct {
 }
 
-// Creates a dint using given year, month and day.
-func (dint Dint) create(year, month, day int) int {
+// Create Creates a dint using given year, month and day.
+func (dint Dint) Create(year, month, day int) int {
 	return year*10000 + month*100 + day
 }
 
-// Creates a dint from time.Time.
-func (dint Dint) createFromTime(date time.Time) int {
-	return dint.create(date.Year(), int(date.Month()), date.Day())
+// CreateFromTime Creates a dint from time.Time.
+func (dint Dint) CreateFromTime(date time.Time) int {
+	return dint.Create(date.Year(), int(date.Month()), date.Day())
 }
 
-// Returns the year of a given dint.
-func (dint Dint) year(year int) int {
+// Compose Composes a dint using any number of years, months and days.
+func (dint Dint) Compose(year, month, day int) int {
+	return dint.AddDays(
+		dint.AddMonths(year*10000, month),
+		day)
+}
+
+// Year Returns the year of a given dint.
+func (dint Dint) Year(year int) int {
 	return year / 10000
 }
 
-// Returns the month of a given dint.
-func (dint Dint) month(month int) int {
+// Month Returns the month of a given dint.
+func (dint Dint) Month(month int) int {
 	return month / 100 % 100
 }
 
-// Returns the day of the month of a given dint.
-func (dint Dint) day(day int) int {
+// Day Returns the day of the month of a given dint.
+func (dint Dint) Day(day int) int {
 	return day % 100
 }
 
-// Creates a time.Time instance from a given dint.
-func (dint Dint) toDate(param int) time.Time {
-	return time.Date(dint.year(param), time.Month(dint.month(param)), dint.day(param), 0, 0, 0, 0, time.UTC)
+// ToDate Creates a time.Time instance from a given dint.
+func (dint Dint) ToDate(param int) time.Time {
+	return time.Date(dint.Year(param), time.Month(dint.Month(param)), dint.Day(param), 0, 0, 0, 0, time.UTC)
 }
 
-// Returns the current date dint.
-func (dint Dint) today() int {
-	return dint.createFromTime(time.Now())
+// Today Returns the current date dint.
+func (dint Dint) Today() int {
+	return dint.CreateFromTime(time.Now())
 }
 
-// Returns a dint at the first day of a month of a given dint.
-func (dint Dint) firstDayOfMonth(param int) int {
-	return dint.create(dint.year(param), dint.month(param), 1)
+// FirstDayOfMonth Returns a dint at the first day of a month of a given dint.
+func (dint Dint) FirstDayOfMonth(param int) int {
+	return dint.Create(dint.Year(param), dint.Month(param), 1)
 }
 
-// Returns a dint at the last day of a month of a given dint.
-func (dint Dint) lastDayOfMonth(param int) int {
-	year, month := dint.year(param), dint.month(param)
-	return dint.create(year, month, dint.daysInAMonth(year, month))
+// LastDayOfMonth Returns a dint at the last day of a month of a given dint.
+func (dint Dint) LastDayOfMonth(param int) int {
+	year, month := dint.Year(param), dint.Month(param)
+	return dint.Create(year, month, dint.DaysInAMonth(year, month))
 }
 
-// Returns the number of days in a given month.
-func (dint Dint) daysInAMonth(year, month int) int {
+// LaysInAMonth Returns the number of days in a given month.
+func (dint Dint) LaysInAMonth(year, month int) int {
 	if month == 2 {
-		if dint.isLeapYear(year) {
+		if dint.IsLeapYear(year) {
 			return 29
 		}
 		return 28
@@ -67,63 +74,74 @@ func (dint Dint) daysInAMonth(year, month int) int {
 	return 31 - ((month - 1) % 7 % 2)
 }
 
-// Returns if a given year is a leap year.
-func (dint Dint) isLeapYear(year int) bool {
+// DaysInAMonth Returns the number of days in a given month.
+func (dint Dint) DaysInAMonth(year, month int) int {
+	if month == 2 {
+		if dint.IsLeapYear(year) {
+			return 29
+		}
+		return 28
+	}
+	return 31 - ((month - 1) % 7 % 2)
+}
+
+// IsLeapYear Returns if a given year is a leap year.
+func (dint Dint) IsLeapYear(year int) bool {
 	return !((year%4 != 0) || ((year%100 == 0) && (year%400 != 0)))
 }
 
-// Returns the amount of days between two given dints.
-func (dint Dint) diff(dint1, dint2 int) int {
-	return dint.toJulianDay(dint1) - dint.toJulianDay(dint2)
+// Diff Returns the amount of days between two given dints.
+func (dint Dint) Diff(dint1, dint2 int) int {
+	return dint.ToJulianDay(dint1) - dint.ToJulianDay(dint2)
 }
 
-// Adds years to a dint. A negative number is allowed.
+// AddYears Adds years to a dint. A negative number is allowed.
 // The resulting dint's number of days will be limited by the number of days in the resulting month.
-func (dint Dint) addYears(param int, years int) int {
-	return dint.composeLimit(param, dint.year(param)+years, dint.month(param))
+func (dint Dint) AddYears(param int, years int) int {
+	return dint.ComposeLimit(param, dint.Year(param)+years, dint.Month(param))
 }
 
-// Adds years to a dint. A negative number is allowed.
+// AddYearsExtend Adds years to a dint. A negative number is allowed.
 // If the dint day is the last day in a month then the resulting day will be the last day in a month as well.
-func (dint Dint) addYearsExtend(param int, years int) int {
-	return dint.composeExtend(param, dint.year(param)+years, dint.month(param))
+func (dint Dint) AddYearsExtend(param int, years int) int {
+	return dint.ComposeExtend(param, dint.Year(param)+years, dint.Month(param))
 }
 
-// Adds months to a given dint. A negative number is allowed.
+// AddMonths Adds months to a given dint. A negative number is allowed.
 // The resulting dint's number of days will be limited by the number of days in the resulting month.
-func (dint Dint) addMonths(param int, months int) int {
-	monthsTotal := dint.year(param)*12 + dint.month(param) - 1 + months
-	return dint.composeLimit(param, monthsTotal/12, monthsTotal%12+1)
+func (dint Dint) AddMonths(param int, months int) int {
+	monthsTotal := dint.Year(param)*12 + dint.Month(param) - 1 + months
+	return dint.ComposeLimit(param, monthsTotal/12, monthsTotal%12+1)
 }
 
-// Adds months to a given dint. A negative number is allowed.
+// AddMonthsExtend Adds months to a given dint. A negative number is allowed.
 // If the dint day is the last day in a month then the resulting day will be the last day in a month as well.
-func (dint Dint) addMonthsExtend(param int, months int) int {
-	monthsTotal := dint.year(param)*12 + dint.month(param) - 1 + months
-	return dint.composeExtend(param, monthsTotal/12, monthsTotal%12+1)
+func (dint Dint) AddMonthsExtend(param int, months int) int {
+	monthsTotal := dint.Year(param)*12 + dint.Month(param) - 1 + months
+	return dint.ComposeExtend(param, monthsTotal/12, monthsTotal%12+1)
 }
 
-// Adds a given number of days to a given dint. A negative number is allowed.
-func (dint Dint) addDays(param int, days int) int {
-	return dint.fromJulianDay(dint.toJulianDay(param) + days)
+// AddDays Adds a given number of days to a given dint. A negative number is allowed.
+func (dint Dint) AddDays(param int, days int) int {
+	return dint.FromJulianDay(dint.ToJulianDay(param) + days)
 }
 
-// Composes a dint with limited day based on the given year and month.
-func (dint Dint) composeLimit(param int, year, month int) int {
-	return dint.create(year, month, int(math.Min(float64(dint.day(param)), float64(dint.daysInAMonth(year, month)))))
+// ComposeLimit Composes a dint with limited day based on the given year and month.
+func (dint Dint) ComposeLimit(param int, year, month int) int {
+	return dint.Create(year, month, int(math.Min(float64(dint.Day(param)), float64(dint.DaysInAMonth(year, month)))))
 }
 
-// Composes a dint with extended day based on the given year and month.
-func (dint Dint) composeExtend(param int, year, month int) int {
-	if dint.daysInAMonth(dint.year(param), dint.month(param)) == dint.day(param) {
-		return dint.create(year, month, dint.daysInAMonth(year, month))
+// ComposeExtend Composes a dint with extended day based on the given year and month.
+func (dint Dint) ComposeExtend(param int, year, month int) int {
+	if dint.DaysInAMonth(dint.Year(param), dint.Month(param)) == dint.Day(param) {
+		return dint.Create(year, month, dint.DaysInAMonth(year, month))
 	}
-	return dint.composeLimit(param, year, month)
+	return dint.ComposeLimit(param, year, month)
 }
 
-// Converts a dint to Julian Day.
-func (dint Dint) toJulianDay(param int) int {
-	year, month, day := dint.year(param), dint.month(param), dint.day(param)
+// ToJulianDay Converts a dint to Julian Day.
+func (dint Dint) ToJulianDay(param int) int {
+	year, month, day := dint.Year(param), dint.Month(param), dint.Day(param)
 
 	a := (14 - month) / 12
 	y := year + 4800 - a
@@ -132,8 +150,8 @@ func (dint Dint) toJulianDay(param int) int {
 	return day + (153*m+2)/5 + 365*y + y/4 - y/100 + y/400 - 32045
 }
 
-// Converts Julian Day to a dint.
-func (dint Dint) fromJulianDay(julianDay int) int {
+// FromJulianDay Converts Julian Day to a dint.
+func (dint Dint) FromJulianDay(julianDay int) int {
 	p := julianDay + 68569
 	q := 4 * p / 146097
 	r := p - (146097*q+3)/4
@@ -146,12 +164,5 @@ func (dint Dint) fromJulianDay(julianDay int) int {
 	M := u + 2 - 12*v
 	D := t - 2447*u/80
 
-	return dint.create(Y, M, D)
-}
-
-// Composes a dint using any number of years, months and days.
-func (dint Dint) compose(year, month, day int) int {
-	return dint.addDays(
-		dint.addMonths(year*10000, month),
-		day)
+	return dint.Create(Y, M, D)
 }
